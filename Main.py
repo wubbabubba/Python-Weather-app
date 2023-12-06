9dc69a32d8e3c1ca553dba0ea33831685d8d0263
Emmanuel <birruetaemmanuel4@gmail.com>
2023-12-06 10:01:22 -0800

    First print statements (got blocked)
    I was trying to import the request module, and received an error in VS, right now i'm looking through stack overflow to see how i'm supposed to import it.

---
+++ Main.py
@@ -0,0 +1,6 @@
+import os
+import json
+from urllib.request import urlopen
+print("Welcome to the weather app, you will be able to find the live weather data for any city")
+cityName = input("What is the name of the city you'd like to choose?")
+print(cityName)
