9fde73351c5fe277e8263a727d21b80ca50cecb9
Emmanuel <birruetaemmanuel4@gmail.com>
2023-12-12 10:06:41 -0800

    Day 7
    I got an api response, tomorrow I'll sort through the response to get the latitude and longitude of the city

--- Main.py
+++ Main.py
@@ -5,3 +5,11 @@ from urllib.request import urlopen
 print("Welcome to the weather app, you will be able to find the live weather data for any city")
 cityName = input("What is the name of the city you'd like to choose?")
 print(cityName)
+response = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=Paris&limit=5&appid=78e3a6f3ae62901bfa2fcc723df53324")
+data = response.json()
+print(data)
+new_json = json.dumps(data, indent=2)
+#lat = data['lat']
+#longitude = data['lon']
+#print(lat)
+#print(longitude)
