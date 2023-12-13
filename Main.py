9ba8c54ffc01ba0ab13b395516ddc7668958a987
Emmanuel <birruetaemmanuel4@gmail.com>
2023-12-13 10:06:13 -0800

    lostt
    Watched youtube videos trying to figure out how to print out the specific data I want but there is data with the same name causing some issues

--- Main.py
+++ Main.py
@@ -5,11 +5,12 @@ from urllib.request import urlopen
 print("Welcome to the weather app, you will be able to find the live weather data for any city")
 cityName = input("What is the name of the city you'd like to choose?")
 print(cityName)
-response = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=Paris&limit=5&appid=78e3a6f3ae62901bfa2fcc723df53324")
+response = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=Paris,FR&limit=5&appid=78e3a6f3ae62901bfa2fcc723df53324")
 data = response.json()
 print(data)
 new_json = json.dumps(data, indent=2)
-#lat = data['lat']
+option1 = data['name'][4]
+#lat = response['lat']
 #longitude = data['lon']
-#print(lat)
+print(option1)
 #print(longitude)
