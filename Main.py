d05c01c35b2251e337048ad568209060795b290e
Emmanuel <birruetaemmanuel4@gmail.com>
2024-01-10 10:06:40 -0800

    Update Main.py
    Figured out how to filter through api response, but now I am trying to learn how to make multiple arrays for each possible city.

--- Main.py
+++ Main.py
@@ -3,18 +3,20 @@ import json
 import requests
 from urllib.request import urlopen
 import array as arr
-print("Welcome to the weather app, you will be able to find the live weather data for any city")
-cityName = input("What is the name of the city you'd like to choose?")
+print("Welcome to the weather app, you will be able to find the live weather data for any city ")
+cityName = input("What is the name of the city you'd like to choose? ")
 print(cityName)
 response = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=Paris,FR&limit=5&appid=78e3a6f3ae62901bfa2fcc723df53324")
 data = response.json()
 
 new_json = json.dumps(data, indent=2)
+
+
 for x in data:  
-    if x == name:
-     print(x)
-options = arr.data('i'[2])
-print(options)
+    print(x['name'][1])
+ #   option1 = [x['name'], x['state'], x['country']]
+  #  print (option1)
+
 #option1 = data['name']
 #lat = response['lat']
 #longitude = data['lon']
