4dc98cbd2931803680fc647402157d88b86854ff
Emmanuel <birruetaemmanuel4@gmail.com>
2024-01-17 10:06:51 -0800

    Got the data for the temperature
    Gonna make this easy to read to the user

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 import array as arr
 print("Welcome to the weather app, you will be able to find the live weather data for any city ")
 cityName = input("What is the name of the city you'd like to choose? ")
-cityCountry = input("What country is the city in? (Use the Abbreviation please)")
+cityCountry = input("What country is the city in? (Use the Abbreviation please) ")
 print(cityName)
 print(cityCountry)
 response = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + cityCountry + "&limit=5&appid=78e3a6f3ae62901bfa2fcc723df53324")
@@ -28,10 +28,22 @@ print(data[choice - 1]['lon'])
 
 print("https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + "78e3a6f3ae62901bfa2fcc723df53324")
 
-response1 = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + "78e3a6f3ae62901bfa2fcc723df53324")
+
+response1 = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + "78e3a6f3ae62901bfa2fcc723df53324&units=imperial")
 
 data1 = response1.json()
-print(data1)
+#print(data1)
+
+
+weather = data1['main']
+
+print("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n" + cityName + " : " + str(weather['temp']) + "°")
+print("Feels like : "  + str(weather['feels_like']) + "°" )
+print("Todays low : " + str(weather['temp_min']) + "°" + "         Today's high : " + str(weather['temp_max']))
+
+print(weather)
+#print(weather) 
+
 
 #option1 = data['name']
 #lat = response['lat']
